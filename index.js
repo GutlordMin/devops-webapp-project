d0b54a4ed89a7c9d6347a5f37ec27264f3693bb4
GutlordMin <amin.kamarudin@s.unikl.edu.my>
2025-01-22 10:00:48 +0800

    updated index.js

--- index.js
+++ index.js
@@ -2,8 +2,20 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+// Existing route (Hello World)
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send(`
+    <h1>Hello World!</h1>
+    <button onclick="window.location.href='/final-assessment'">Go to Final Assessment</button>
+  `);
+});
+
+// New route for Final Assessment Page
+app.get('/final-assessment', (req, res) => {
+  res.send(`
+    <h1>Our Work as the Final Assessment for Software Engineering</h1>
+    <p>We love UniKL!</p>
+  `);
 });
 
 app.listen(port, () => {
